9c6d8af921a460edc29ba96028f3646124291159
Michał D <michal@datarevenue.com>
2021-02-11 14:40:57 +0100

    Change root_volume_type of nodes to gp2
    AWS complained to me that it cannot be gp3. I don't know
    why it worked for Pedro.

--- main.tf
+++ main.tf
@@ -61,11 +61,11 @@ module "eks" {
       instance_type                 = "t3.large"
       additional_userdata           = ""
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      root_volume_type              = "gp2"
 
       #autoscaling group section
       asg_max_size         = "2"
       asg_desired_capacity = "2"
-
     },
 
   ]
