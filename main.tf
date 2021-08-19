95a5f53d50c7629d6cc7157514d8e0454db86482
Pat Thontirawong <pat@datarevenue.com>
2021-08-19 15:52:09 -0400

    force terraform vpc version to 3.6.0

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
+  version              = "3.6.0"
   name                 = var.cluster_name
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
