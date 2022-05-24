a0b22001254f4a10da3dee142437e726de21f6ae
Thales <thales@datarevenue.com>
2022-05-24 10:29:03 -0300

    Fix EKS version

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.1.0"
   cluster_name    = var.cluster_name
   cluster_version = var.kubernetes_version
   subnets         = module.vpc.private_subnets
